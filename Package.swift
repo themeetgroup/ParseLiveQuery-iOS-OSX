9f4e364ade01759de9572ce5aaefbd097217e042
Jedidiah Laudenslayer <jlaudenslayer@themeetgroup.com>
2024-04-13 15:57:30 -0400

    SNAP-920 Update package manifest
    Change-Id: I22e4b7b46e35460f283b64bae4a3d4f15304480d

--- Package.swift
+++ Package.swift
@@ -9,54 +9,34 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Starscream",
-            targets: ["StarscreamWrapper"]),
+            targets: ["Starscream"]),
         .library(
             name: "ParseCore",
-            targets: ["ParseCoreWrapper"]),
+            targets: ["ParseCore", "Bolts"]),
         .library(
             name: "TMGParseLiveQuery",
-            targets: ["TMGParseLiveQueryWrapper"]),
+            targets: ["TMGParseLiveQuery", "BoltsSwift"]),
     ],
     targets: [
-        .target(name: "StarscreamWrapper",
-                dependencies: [
-                    .target(name: "Starscream"),
-                ]),
-        .target(name: "ParseCoreWrapper",
-                dependencies: [
-                    .target(name: "ParseCore"),
-                    .target(name: "Bolts"),
-                ]),
-        .target(name: "TMGParseLiveQueryWrapper",
-                dependencies: [
-                    .target(name: "TMGParseLiveQuery"),
-                    .target(name: "BoltsSwift"),
-                ]),
-        // Parse Dependencies
         .binaryTarget(
             name: "ParseCore",
-            url: "https://github.com/themeetgroup/ParseLiveQuery-iOS-OSX/raw/xcframework/Frameworks/ParseCore.zip",
-            checksum: "cea6d5b2a16b614a3c1ba07f22f2a412a9785819e3c6facb9ce4ea00db1ac200"
+            path: "Frameworks/ParseCore.xcframework"
         ),
         .binaryTarget(
             name: "TMGParseLiveQuery",
-            url: "https://github.com/themeetgroup/ParseLiveQuery-iOS-OSX/raw/xcframework/Frameworks/TMGParseLiveQuery.zip",
-            checksum: "4f2fc26274cff837bf2f07cfb51a72b57c96264299345bb0963749c173ff0d87"
+            path: "Frameworks/TMGParseLiveQuery.xcframework"
         ),
         .binaryTarget(
             name: "Starscream",
-            url: "https://github.com/themeetgroup/ParseLiveQuery-iOS-OSX/raw/d3dd16dd76bd8d686cb6eabae96ac855e1a09804/Frameworks/Starscream.zip",
-            checksum: "07c9eee55d96924fec04ed5bde6386e02f4dc227d0cd9a7622365370aac6fbb3"
+            path: "Frameworks/Starscream.xcframework"
         ),
         .binaryTarget(
             name: "Bolts",
-            url: "https://github.com/themeetgroup/ParseLiveQuery-iOS-OSX/raw/xcframework/Frameworks/Bolts.zip",
-            checksum: "266ef2e24acfe57f97dc6638f870c0173f98e24a8bd2b5bd7fbf877c60618528"
+            path: "Frameworks/Bolts.xcframework"
         ),
         .binaryTarget(
             name: "BoltsSwift",
-            url: "https://github.com/themeetgroup/ParseLiveQuery-iOS-OSX/raw/xcframework/Frameworks/BoltsSwift.zip",
-            checksum: "4e64e4db55e9eac38d3a75a1462a87a1de250c97491489d84b43cb896585e935"
+            path: "Frameworks/BoltsSwift.xcframework"
         ),
     ]
 )
